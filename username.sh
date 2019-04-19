17bfd92aea60393548826bafe4f9c8177a386ef8
Joshua Barrs <barrs@chapman.edu>
2019-04-19 10:43:48 -0700

    Committing

--- username.sh
+++ username.sh
@@ -1,18 +1,19 @@
 #! /bin/bash
 # zip.sh
 # author
-echo "Enter a username: "
-echo "The only characters that can be used are: "
-echo "Lower case letters"
-echo "digits"
-echo "the underscore character" 
-echo "The username must start with a lower case letter" 
-echo "The username must contain at least 3 but no more than 12 characters"
-read ZIP
-while echo "$ZIP" | egrep -v "^[a-z][0-9][_]{3,12}$" > /dev/null 2>&1
+echo "Enter a valud username"
+echo "Valid username can only use: "
+echo "Lower case letters" 
+echo "Digits" 
+echo "The underscore character" 
+echo "It must start with a lower case letter"
+echo "It must contain at least three but no more than 12 characters" 
+
+read username
+while echo "$username" | egrep -v "^[a-z][a-z_0-9]{2,11}$" > /dev/null 2>&1
 do
-	echo "You must enter a valid ZIP code - five digits only!"
-	echo "Enter a five-digit ZIP code: "
-	read ZIP
+	echo "You must enter a valid username - follow the rules above!"
+	echo "Enter a valid username: "
+	read username
 done
 echo "Thank you"
